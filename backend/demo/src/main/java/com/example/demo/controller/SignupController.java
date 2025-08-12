2da868b83cac2fa8395d405494fa21e7efb15c1e
kichan kim <kkc7130coding@gmail.com>
2025-08-12 14:50:23 +0900

    signup&in

--- backend/demo/src/main/java/com/example/demo/controller/SignupController.java
+++ backend/demo/src/main/java/com/example/demo/controller/SignupController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.demo.service.UserService;
+import com.example.demo.dto.SignupDto;
+
 @Controller
 @RequiredArgsConstructor
 public class SignupController {
